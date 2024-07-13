72f3e8dcddcf39f6555b279cf3e428ce546ac06f
Madhuri <madhuridighe2205@gmail.com>
2024-07-13 17:21:45 +0530

    added gitEx and modified Anagram progm

--- src/test/java/Practice/Anagram.java
+++ src/test/java/Practice/Anagram.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Anagram {
 
 	public static void main(String[] args) {
+		String git1="Learning git";
+		String git2="Learning git commit";
 		String s="dog";
 		String s1="gor";
 		char arr[]=s.toCharArray();
